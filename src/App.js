0948c77e8ee75a2a07e87bb3e09d6fabb5aa5329
Hamza <hamza.ali5542@gmail.com>
2018-03-26 21:15:11 +0500

    handling events through methods

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Person from './Person/Person'
 class App extends Component {
+  //state present in component, can only be used inside extending component's class
+  state = {
+    persons: [
+      {name:'Hamza', age:22},
+      {name:'Ali', age: 22},
+      {name:'Mohsin', age:23}
+    ]
+  }
+  switchNameHandler = () => {
+    console.log('was clicked')
+
+  }
   render() {
-    //state present in component, can only be used inside extending component's class
-    state = {
-      persons: [
-        {name:'Hamza', age:22},
-        {name:'Ali', age: 22},
-        {name:'Mohsin', age:23}
-      ]
-    }
+    
+    
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React JS</h2>
         </div>
+        <button onClick={this.switchNameHandler}>SwitchName</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Some additional info</Person>
